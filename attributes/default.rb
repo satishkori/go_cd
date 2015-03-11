3539fce6478526f8ed9cf359a02a07909eb2c65e
Ranjib Dey <ranjib@linux.com>
2015-03-10 22:49:02 -0700

    bump up server versionb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['go_cd']['server_download_url'] = 'http://download.go.cd/gocd-deb/go-server-14.2.0-377.deb'
+default['go_cd']['server_download_url'] = 'http://download.go.cd/gocd-deb/go-server-14.4.0-1356.deb'
 default['go_cd']['agent_download_url'] = 'http://download.go.cd/gocd/go-agent-14.4.0-1356.zip'
 default['go_cd']['user'] = 'go'
 default['go_cd']['group'] = 'go'
